28e905219754ad0c51acb18cd3919b4acc2ae238
daixl <daixl@ct108.com>
2023-08-17 15:25:26 +0800

    修复 打包bug

---
+++ template/build-user/tool.ts
@@ -0,0 +1,53 @@
+const buildConfig = require('./config');
+import { globSync } from 'glob';
+
+export type IEnv = 'stable' | 'dev' | 'ctest' | 'pre' | 'production' | 'development';
+
+export const getEnvConfig = (viteEnv: IEnv) => {
+  return buildConfig.envConfig[viteEnv as IEnv];
+};
+
+// 设置base
+export const getBase = (viteEnv: IEnv) => {
+  return viteEnv === 'development' ? '/' : getEnvConfig(viteEnv).domainUrl + buildConfig.appUrl;
+};
+
+interface IPageConfig {
+  entry: string; //入口文件
+  filename: string; //指定打包输出的index.html路径
+  template: string; //模版页面
+}
+interface IPages {
+  [key: string]: string;
+}
+const getMulu = function (filePath: string) {
+  const filename1 = filePath.substring(0, filePath.lastIndexOf('\\'));
+  const filename2 = filename1.substring(filename1.lastIndexOf('\\') + 1);
+  return filename2;
+};
+// 多页配置
+export const getPages = () => {
+  const pages: IPages = {
+    //index: 'src/pages/index.html'
+  };
+  try {
+    const indexFile = globSync('src/pages/index.html');
+    if (indexFile.length > 0) {
+      pages['index'] = 'src/pages/index.html';
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  try {
+    const entryFiles = globSync('src/pages/*/index.html');
+
+    entryFiles.forEach((filePath: string) => {
+      const filename = getMulu(filePath);
+      pages[filename] = filePath.replace(/\\/g, '/');
+    });
+  } catch (e) {
+    console.log(e);
+  }
+  console.log('pages===', pages);
+  return pages;
+};
--- templete/src/stores/store.ts
+++
@@ -1,7 +0,0 @@
-import { createPinia } from 'pinia';
-import { useStore } from '@/stores';
-
-const pinia = createPinia();
-
-const store = useStore(pinia);
-export default store;
